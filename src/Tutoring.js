dc0f9435094691a0e879015b669d5a6bf7222871
DaKahd <90017411+DaKahd@users.noreply.github.com>
2024-07-02 16:06:28 -0700

    Updated tutoring form to fit mobile

--- src/Tutoring.js
+++ src/Tutoring.js
@@ -35,7 +35,9 @@ export function Contact(){
     <div className="text">   
         <h1>Contact</h1>
         <p>I offer one free one-hour session per student. Contact me for pricing and availability.</p><br/><br/>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdkHsOVpSFanBj0lIFl7oWr-PRqLapgobrETbX_vWpAJwMgwQ/viewform?embedded=true" width="600" height="1433" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        <div id="form">
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdkHsOVpSFanBj0lIFl7oWr-PRqLapgobrETbX_vWpAJwMgwQ/viewform?embedded=true" width="100%" height="1700" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        </div>
         </div>
     )
 }
